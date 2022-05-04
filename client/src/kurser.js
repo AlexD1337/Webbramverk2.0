9558d7b22fef2b304bfd6b1fd0c33bc29b7ae468
Alex Danil <70381167+AlexD1337@users.noreply.github.com>
2022-05-04 12:10:28 +0200

    Update kurser.js

--- client/src/kurser.js
+++ client/src/kurser.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './index.css'
+
+// Ganni
 function Kurser() {
     const [name, setName] = useState(null)
     const [length, setLength] = useState(0)
